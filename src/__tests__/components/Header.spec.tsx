72f69eb68e4f125f3f9f6116a4a0799fe084f679
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-04-11 15:21:16 -0300

    Adicionado a linha faltante no test

--- src/__tests__/components/Header.spec.tsx
+++ src/__tests__/components/Header.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react'
 import { ReactNode } from 'react'
 import Header from '../../components/Header'
+import '@testing-library/jest-dom/extend-expect'
 
 jest.mock('react-router-dom', () => {
   return {
